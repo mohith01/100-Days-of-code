347cefad42f6d0c0fda1269928c34bf6d447fc17
Mohit Sai Aravind <42903811+mohith01@users.noreply.github.com>
2021-06-08 10:49:00 +0530

    Create 1528.py

---
+++ py/1528.py
@@ -0,0 +1,7 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        res= ''
+        for i in range(len(indices)):
+            res+=s[indices.index(i)]
+            
+        return res
