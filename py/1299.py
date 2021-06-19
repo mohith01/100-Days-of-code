b381984645840a65bbe8e0941d607a259df87162
Mohit Sai Aravind <42903811+mohith01@users.noreply.github.com>
2021-06-19 22:34:32 +0530

    Create 1299.py

---
+++ py/1299.py
@@ -0,0 +1,8 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        res= []
+        for i in range(len(arr)-1):
+            res.append(max(arr[i+1:]))
+            
+        res.append(-1)
+        return res
